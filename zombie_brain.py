7c7df3b89dd042d3bbe6efce5c92cd1f340fb50e
Christian Conti <chri@AirdiChristian.homenet.telecomitalia.it>
2024-05-20 19:08:16 +0200

    added best score and setup new game

--- zombie_brain.py
+++ zombie_brain.py
@@ -1,204 +1,227 @@
 import pygame
 import random
 
-# Inizializza Pygame
-pygame.init()
 
-# Imposta le dimensioni della finestra
-window_width = 800
-window_height = 600
+def best_score(score, best_try):
+    if score > best_try:
+        best_try = score
+        return best_try
+    return best_try
 
-window = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption("Zombie Brains")
+best_try = 0
+score = 0
+quit_game = False
+while not quit_game:
+    best_try = best_score(score, best_try)
+    # Inizializza Pygame
+    pygame.init()
 
-# Definisci i colori
-black = (0, 0, 0)
-green = (0, 255, 0)
-red = (255, 0 ,0)
+    # Imposta le dimensioni della finestra
+    window_width = 800
+    window_height = 600
+
+    window = pygame.display.set_mode((window_width, window_height))
+    pygame.display.set_caption("Zombie Brains")
+
+    # Definisci i colori
+    black = (0, 0, 0)
+    green = (0, 255, 0)
+    red = (255, 0, 0)
+
+    # Carica le immagini
+    zombie_head_up = pygame.image.load("assets/block_part/zombie_head_up.png")
+    zombie_head_up = pygame.transform.scale(zombie_head_up, (20, 20))
+    zombie_head_down = pygame.image.load("assets/block_part/zombie_head_down.png")
+    zombie_head_down = pygame.transform.scale(zombie_head_down, (20, 20))
+    zombie_head_left = pygame.image.load("assets/block_part/zombie_head_left.png")
+    zombie_head_left = pygame.transform.scale(zombie_head_left, (20, 20))
+    zombie_head_right = pygame.image.load("assets/block_part/zombie_head_right.png")
+    zombie_head_right = pygame.transform.scale(zombie_head_right, (20, 20))
+    brain = pygame.image.load("assets/block_part/brain.png")
+    brain = pygame.transform.scale(brain, (20, 20))
+    wall = pygame.image.load("assets/block_part/wall.png")
+    wall = pygame.transform.scale(wall, (20, 20))
+
+    # Definisci le dimensioni dello zombie, del muro e del cervello
+    zombie_size = 20
+    brain_size = 20
+    wall_size = 20
+
+    # Inizializza il font per il punteggio e per il punteggio finale
+    font_path = "assets/fonts/youmurdererbb_reg.ttf"
+    font_size = 36
+    font = pygame.font.Font(font_path, font_size)
+    end_font = pygame.font.Font(font_path, 50)
 
 
-# Carica le immagini
-zombie_head_up = pygame.image.load("assets/block_part/zombie_head_up.png")
-zombie_head_up = pygame.transform.scale(zombie_head_up, (20, 20))
-zombie_head_down = pygame.image.load("assets/block_part/zombie_head_down.png")
-zombie_head_down = pygame.transform.scale(zombie_head_down, (20, 20))
-zombie_head_left = pygame.image.load("assets/block_part/zombie_head_left.png")
-zombie_head_left = pygame.transform.scale(zombie_head_left, (20, 20))
-zombie_head_right = pygame.image.load("assets/block_part/zombie_head_right.png")
-zombie_head_right = pygame.transform.scale(zombie_head_right, (20, 20))
-brain = pygame.image.load("assets/block_part/brain.png")
-brain = pygame.transform.scale(brain, (20, 20))
-wall = pygame.image.load("assets/block_part/wall.png")
-wall = pygame.transform.scale(wall, (20, 20))
+    # Definisci la posizione iniziale dello zombie
+    zombie_x = window_width // 2
+    zombie_y = window_height // 2
 
-# Definisci le dimensioni dello zombie, del muro e del cervello
-zombie_size = 20
-brain_size = 20
-wall_size = 20
+    # Definisci il vettore di movimento iniziale
+    x_change = 0
+    y_change = 0
 
-# Definisci la posizione iniziale dello zombie
-zombie_x = window_width // 2
-zombie_y = window_height // 2
-
-# Definisci il vettore di movimento iniziale
-x_change = 0
-y_change = 0
-
-
-# Crea cervello e crea muro iniziale
-brain_x = random.randrange(brain_size, window_width - brain_size, brain_size)
-brain_y = random.randrange(brain_size, window_height - brain_size, brain_size)
-wall_x = random.randrange(wall_size, window_width - wall_size, wall_size)
-wall_y = random.randrange(wall_size, window_height - wall_size, wall_size)
-
-# Controlla se il muro è stato generato nella stessa posizione dello zombie
-while wall_x == zombie_x and wall_x == zombie_y:
+    # Crea cervello e crea muro iniziale
+    brain_x = random.randrange(brain_size, window_width - brain_size, brain_size)
+    brain_y = random.randrange(brain_size, window_height - brain_size, brain_size)
     wall_x = random.randrange(wall_size, window_width - wall_size, wall_size)
     wall_y = random.randrange(wall_size, window_height - wall_size, wall_size)
 
-walls = [(wall_x, wall_y)]
+    # Controlla se il muro è stato generato nella stessa posizione dello zombie
+    while wall_x == zombie_x and wall_x == zombie_y:
+        wall_x = random.randrange(wall_size, window_width - wall_size, wall_size)
+        wall_y = random.randrange(wall_size, window_height - wall_size, wall_size)
 
-# Inizializza il punteggio e la velocità e i cervelli presi
-score = 0
-speed = 8
-brain_taken = 0
-final_score = 0
+    walls = [(wall_x, wall_y)]
 
-#funzione per generare il cervello
-def generate_brain_position():
-    brain_x = random.randrange(brain_size, window_width - brain_size, brain_size)
-    brain_y = random.randrange(brain_size, window_height - brain_size, brain_size)
+    # Inizializza il punteggio e la velocità e i cervelli presi
+    score = 0
+    speed = 8
+    brain_taken = 0
 
-    # Verificare se la posizione del cervello non collide con i muri esistenti
-    while any((brain_x == wall_x and brain_y == wall_y) for wall_x, wall_y in walls):
+
+
+
+    # funzione per generare il cervello
+    def generate_brain_position():
         brain_x = random.randrange(brain_size, window_width - brain_size, brain_size)
         brain_y = random.randrange(brain_size, window_height - brain_size, brain_size)
 
-    return brain_x, brain_y
+        # Verificare se la posizione del cervello non collide con i muri esistenti
+        while any((brain_x == wall_x and brain_y == wall_y) for wall_x, wall_y in walls):
+            brain_x = random.randrange(brain_size, window_width - brain_size, brain_size)
+            brain_y = random.randrange(brain_size, window_height - brain_size, brain_size)
+
+        return brain_x, brain_y
 
 
-# Funzione per disegnare il gioco
-def draw_game():
-    # Cancella la finestra
-    window.fill(black)
+    # Funzione per disegnare il gioco
+    def draw_game():
+        # Cancella la finestra
+        window.fill(black)
 
-    # Disegna il punteggio
-    score_text = font.render("Score: " + str(score), True, green)
-    window.blit(score_text, (10, 10))
+        # Disegna il punteggio
+        score_text = font.render("Score: " + str(score) + " Best: " + str(best_try), True, green)
+        window.blit(score_text, (10, 10))
 
-    # Disegna lo zombie
-    if x_change == 0 and y_change == -zombie_size:
-        window.blit(zombie_head_up, (zombie_x, zombie_y))
-    elif x_change == 0 and y_change == zombie_size:
-        window.blit(zombie_head_down, (zombie_x, zombie_y))
-    elif x_change == -zombie_size and y_change == 0:
-        window.blit(zombie_head_left, (zombie_x, zombie_y))
-    elif x_change == zombie_size and y_change == 0:
-        window.blit(zombie_head_right, (zombie_x, zombie_y))
+        # Disegna lo zombie
+        if x_change == 0 and y_change == -zombie_size:
+            window.blit(zombie_head_up, (zombie_x, zombie_y))
+        elif x_change == 0 and y_change == zombie_size:
+            window.blit(zombie_head_down, (zombie_x, zombie_y))
+        elif x_change == -zombie_size and y_change == 0:
+            window.blit(zombie_head_left, (zombie_x, zombie_y))
+        elif x_change == zombie_size and y_change == 0:
+            window.blit(zombie_head_right, (zombie_x, zombie_y))
 
-    # Disegna il cervello
-    window.blit(brain, (brain_x, brain_y))
+        # Disegna il cervello
+        window.blit(brain, (brain_x, brain_y))
 
-    # Disegna i muri
-    for wall_x, wall_y in walls:
-        window.blit(wall, (wall_x, wall_y))
+        # Disegna i muri
+        for wall_x, wall_y in walls:
+            window.blit(wall, (wall_x, wall_y))
 
-    # Aggiorna il display
-    pygame.display.update()
+        # Aggiorna il display
+        pygame.display.update()
 
 
-# Inizializza il font per il punteggio e per il punteggio finale
-font_path = "assets/fonts/youmurdererbb_reg.ttf"
-font_size = 36
-font = pygame.font.Font(font_path, font_size)
-end_font = pygame.font.Font(font_path, 50)
+    # Inizializza il clock di Pygame
+    clock = pygame.time.Clock()
 
-# Inizializza il clock di Pygame
-clock = pygame.time.Clock()
+    # Game loop
+    game_over = False
+    while not game_over:
+        # Gestisci gli eventi
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                    x_change = -zombie_size
+                    y_change = 0
+                elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                    x_change = zombie_size
+                    y_change = 0
+                elif event.key == pygame.K_w or event.key == pygame.K_UP:
+                    x_change = 0
+                    y_change = -zombie_size
+                elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                    x_change = 0
+                    y_change = zombie_size
 
-# Game loop
-game_over = False
-while not game_over:
-    # Gestisci gli eventi
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        # Muovi zombie
+        zombie_x += x_change
+        zombie_y += y_change
+
+        # Controlla le collisioni con i bordi della finestra
+        if zombie_x < 0 or zombie_x >= window_width or zombie_y < 0 or zombie_y >= window_height:
             game_over = True
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                x_change = -zombie_size
-                y_change = 0
-            elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                x_change = zombie_size
-                y_change = 0
-            elif event.key == pygame.K_w or event.key == pygame.K_UP:
-                x_change = 0
-                y_change = -zombie_size
-            elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                x_change = 0
-                y_change = zombie_size
 
-    # Muovi zombie
-    zombie_x += x_change
-    zombie_y += y_change
+        # Controlla le collisioni con il corpo dello zombie con il muro
+        for wall_x, wall_y in walls:
+            if zombie_x == wall_x and zombie_y == wall_y:
+                game_over = True
+                break
 
-    # Controlla le collisioni con i bordi della finestra
-    if zombie_x < 0 or zombie_x >= window_width or zombie_y < 0 or zombie_y >= window_height:
-        game_over = True
+        # Controlla se lo zombie ha mangiato il cervello e genera un muro random
+        if zombie_x == brain_x and zombie_y == brain_y:
+            wall_x = random.randrange(wall_size, window_width - wall_size, wall_size)
+            wall_y = random.randrange(wall_size, window_height - wall_size, wall_size)
+            brain_x, brain_y = generate_brain_position()
 
-    # Controlla le collisioni con il corpo dello zombie con il muro
-    for wall_x, wall_y in walls:
-        if zombie_x == wall_x and zombie_y == wall_y:
-            game_over = True
-            break
+            walls.append((wall_x, wall_y))  # Aggiungi nuove coordinate del muro come tupla
 
-    # Controlla se lo zombie ha mangiato il cervello e genera un muro random
-    if zombie_x == brain_x and zombie_y == brain_y:
-        wall_x = random.randrange(wall_size, window_width - wall_size, wall_size)
-        wall_y = random.randrange(wall_size, window_height - wall_size, wall_size)
-        brain_x, brain_y = generate_brain_position()
+            brain_taken += 1
 
-        walls.append((wall_x, wall_y))  # Aggiungi nuove coordinate del muro come tupla
+            if brain_taken < 10:
+                score += 5
+            elif brain_taken < 20:
+                score += 15
+            elif brain_taken < 40:
+                score += 40
+            elif brain_taken > 40:
+                score += 50
 
-        brain_taken += 1
+            best_try = best_score(score, best_try)
+        # Disegna il gioco
+        draw_game()
 
-        if brain_taken < 10:
-            score += 5
-        elif brain_taken < 20:
-            score += 15
-        elif brain_taken < 40:
-            score += 40
-        elif brain_taken > 40:
-            score += 50
+        # Regola la velocità del gioco
+        clock.tick(speed)
 
-        final_score = score
-    # Disegna il gioco
-    draw_game()
-
-    # Regola la velocità del gioco
-    clock.tick(speed)
-
-# Schermata di punteggio finale
-close_game = False
-while not close_game:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            close_game = True
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN:
+    # Schermata di punteggio finale
+    close_game = False
+    while not close_game:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 close_game = True
+                quit_game = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    close_game = True
+                    quit_game = True
+                elif event.key == pygame.K_r:
+                    close_game = True
+                    quit_game = False
 
-    text_score = end_font.render("Final Score: " + str(final_score), True, (red))
-    text_quit = end_font.render("Press Return to Quit", True, (red))
+        text_score = end_font.render("Final Score: " + str(score), True, (red))
+        text_best_score = end_font.render("The best Score: " + str(best_try), True, (red))
+        text_quit = end_font.render("Press Return to Quit", True, (red))
+        text_replay = end_font.render("Or press R to Replay", True, (red))
 
-    # Ricrea la schemata di gioco per poi metterci il punteggio
-    window.fill(black)
-    window.blit(brain, (brain_x, brain_y))
-    for wall_x, wall_y in walls:
-        window.blit(wall, (wall_x, wall_y))
+        # Ricrea la schemata di gioco per poi metterci il punteggio
+        window.fill(black)
+        window.blit(brain, (brain_x, brain_y))
+        for wall_x, wall_y in walls:
+            window.blit(wall, (wall_x, wall_y))
 
-    window.blit(text_score, (window_width // 2, window_height // 2))
-    window.blit(text_quit, (window_width // 2, window_height //2 + 60))
-    pygame.display.update()
+        window.blit(text_score, (window_width // 2, window_height // 2))
+        window.blit(text_best_score, (window_width // 2, window_height // 2 + 60))
+        window.blit(text_quit, (window_width // 2, window_height // 2 + 120))
+        window.blit(text_replay, (window_width // 2, window_height // 2 + 180))
+        pygame.display.update()
 
-# Quit Pygame
-pygame.quit()
+    # Quit Pygame
+    pygame.quit()
+
